8d3e7d222622df98d8a693325ec37d620f5bc16c
Scott Pullen <s.pullen05@gmail.com>
2016-02-22 20:02:26 -0500

    Getting viewer to open from source other than clicking directly on image

--- scripts/app.js
+++ scripts/app.js
@@ -16,24 +16,14 @@ $(function() {
           infinite: true,
           dots: true
         }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ]
+  });
+
+  $('.open-viewer').on('click', function(e) {
+    e.preventDefault();
+
+    var $button = $(this);
+    $button.prev('img').click();
   });
 });
